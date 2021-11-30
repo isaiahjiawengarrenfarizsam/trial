f07d360e1b7220f27ed1b65e311abc9663b1c9b8
isaiahjiawengarrenfarizsam <94510388+isaiahjiawengarrenfarizsam@users.noreply.github.com>
2021-11-30 16:51:22 +0800

    Update TestGit.java

--- JavaApplication7/src/TestGit/TestGit.java
+++ JavaApplication7/src/TestGit/TestGit.java
@@ -14,7 +14,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("garren kontol");
+        System.out.println("garren kontol testLu");
     }
     
 }
